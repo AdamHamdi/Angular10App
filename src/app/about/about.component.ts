e0b60521b4dc64464098443fffc8910d53f71bfc
Adam Hamdi <prodev.webdesign@gmail.com>
2021-03-02 17:09:29 +0100

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+info= {
+  nom:"med",
+  email:"med@gmail.com",
+  tel:6543216
+}
+comments=[
+    {date: new Date(), message : "A"},
+    {date: new Date(), message : "B"},
+    {date: new Date(), message : "C"},
+];
+commentaire={date:null ,message:""};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onAddCommentaire(){
+    // pour affecter la date
+    this.commentaire.date=new Date();
+    //remplir les donnees de l'input dans un tableau
+    this.comments.push(this.commentaire);
+    //pour initialiser le input
+    this.commentaire={date:null, message:""};
+  }
+
+}
