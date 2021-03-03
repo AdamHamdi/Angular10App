1f17991e687363a7f1f3dc3dbbb673090fd28607
Adam Hamdi <prodev.webdesign@gmail.com>
2021-03-03 15:55:04 +0100

    second  commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
   {path:'about', component:AboutComponent},
   {path:'contact', component:ContactsComponent},
+  {path:'gallery', component:GalleryComponent},
   {path:'',redirectTo:'/about', pathMatch:'full'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
 
 
 exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FormsModule } from '@angular/forms';
 import { AboutService } from './services/about.service';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactsComponent
+    ContactsComponent,
+    GalleryComponent
   ],
   imports: [
 
